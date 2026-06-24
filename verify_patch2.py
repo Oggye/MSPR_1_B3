303a7b4acc59618a52fbfb66a0a19e32d7d644a0
Nafissatou SAMB <loadjianaffissatou@gmail.com>
2026-06-24 23:34:05 +0200

    Update Phase 8

--- verify_patch2.py
+++
@@ -1,5 +0,0 @@
-import json
-import pathlib
-path = pathlib.Path(r'C:\Users\loadj\MSPR_1_B3\ia\src\ml\notebooks\03_explicabilite.ipynb')
-nb = json.loads(path.read_text(encoding='utf-8'))
-print(''.join(nb['cells'][1]['source']))
